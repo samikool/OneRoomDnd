29fc80b9cb705c4855dd4d1967ed58f0662b56ed
samikool <sam.morgan44@gmail.com>
2021-03-03 16:33:20 -0600

    crawled api so dnd api is now served from a JSON instead of a double fetch

--- backend/routes/dndAPI.js
+++ backend/routes/dndAPI.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
-const fetch = require('node-fetch')
-const config = require('../constants').config()
+const data = require('../data/master.json')
+
 
 router.get('/', async (req,res) => {
-    let url = config.dndURL + req.baseUrl.replace('/dndAPI', '')
-    let data = await fetch(url)
-    data = await data.json()
-    res.status(200).json(data)
+    let url = req.baseUrl.replace('/dndAPI', '')
+    // console.log(url)
+
+    res.status(200).json(data[url])
 })
 module.exports = router;
